ef58ef173c9756b3a044c54b3494e7687acb9c35
voidarchive <voidarchived@gmail.com>
2022-03-31 15:06:57 +0545

    main loop

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from ipobot import MeroShare
 import json
 
+# Number of Kitta than you want to apply. (Default 10)
 APPLIED_KITTA = 10
-
-
+# Call Meroshare class from ipobot
 meroshare = MeroShare()
 
-# meroshare.login(data.DP,data.Username, data.Password)
-# meroshare.find_ipo()
-# meroshare.apply_ipo(APPLIED_KITTA, data.crn)
-# meroshare.enter_pin(data.pin)
-# meroshare.logout()
+# Load JSON data file
+file = open("data.json")
+data = json.load(file)
+
+# Loop until last object in json file
+for i in range(len(data)): 
+    try:
+        user = data[i] 
+        meroshare.login(user["DP"],user["Username"], user["Password"])
+        meroshare.find_ipo()
+        # meroshare.apply_ipo(APPLIED_KITTA, data.crn)
+        # meroshare.enter_pin(data.pin)
+        meroshare.logout()
+    except:
+        print(f"Error on {user['Name']}")
+
