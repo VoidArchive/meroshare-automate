bae8cf95aa4564ae225c0da70b8a4b02e722b71f
voidarchive <voidarchived@gmail.com>
2022-03-31 14:46:22 +0545

    main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from ipobot import MeroShare
+import json
+
+APPLIED_KITTA = 10
+
+
+meroshare = MeroShare()
+
+# meroshare.login(data.DP,data.Username, data.Password)
+# meroshare.find_ipo()
+# meroshare.apply_ipo(APPLIED_KITTA, data.crn)
+# meroshare.enter_pin(data.pin)
+# meroshare.logout()
